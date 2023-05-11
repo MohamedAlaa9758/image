1c3066ece3000e5e309988c734d254b15866e5fe
mohamed <ma2612557@gmail.con>
2023-05-11 19:44:53 +0300

    new

---
+++ lib/Login_Plants.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'Loginpage.dart';
+import 'SginUp.dart';
+
+
+
+
+class LoginPlants extends StatelessWidget {
+  static const String loginPlants = 'Login_Plants';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Welcome to Detection of plant diseases '),
+      // ),
+      body: Column(
+        children: [
+          Container(
+            child: Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('image/welcomepage2.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+
+                  //#########################
+
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        child: Text(' Log In '),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginPage(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.all(20.0),
+                          fixedSize: Size(200, 70),
+                          textStyle: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                          primary: Colors.green,
+                          onPrimary: Colors.white,
+                          elevation: 15,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(40)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Column(
+                        children: [
+                          ElevatedButton(
+                            child: Text(' Sgin Up '),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SginupPage(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.all(20.0),
+                              fixedSize: Size(200, 70),
+                              textStyle: TextStyle(
+                                  fontSize: 25, fontWeight: FontWeight.bold),
+                              primary: Colors.green,
+                              onPrimary: Colors.white,
+                              elevation: 15,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40)),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 31,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Scanar_bage.dart
+++ lib/Scanar_bage.dart
@@ -1,132 +1,171 @@
-  import 'package:flutter/cupertino.dart';
+  import 'dart:ui';
+import 'package:flutter/cupertino.dart';
   import 'package:flutter/material.dart';
+import 'package:image_processing/main.dart';
   import'Result_scanar.dart';
 
-  class ScanarBage extends StatelessWidget {
-    static const String screenRoute = 'Scanar_bage';
+  class ScanarPage extends StatelessWidget {
+    static const String screenRoute = 'Scanar_Page';
+
     @override
     Widget build(BuildContext context) {
       return Scaffold(
-        backgroundColor: Colors.white10,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                Row(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.white38,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          titleSpacing: 20.0,
+          automaticallyImplyLeading: false,
+          title: Row(
+            children: [
+              Text(
+                'Hello: Username',
+                style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              // SizedBox(
+              //   width: 30.0,
+              // ),
+              // Container(
+              //   decoration: BoxDecoration(
+              //     borderRadius: BorderRadius.circular(20.0),
+              //     color: Colors.green,
+              //   ),
+              //   height: 40.0,
+              //   child: TextButton(
+              //     onPressed: () {},
+              //     child: Text(
+              //       'Contact Us',
+              //       style: TextStyle(
+              //         color: Colors.white,
+              //         fontSize: 17.0,
+              //       ),
+              //     ),
+              //   ),
+              // ),
+            ],
+          ),
+          actions: [
+            IconButton(
+              onPressed: (){},
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),)
+          ],
+        ),
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('image/omar.png.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          //#########################
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 10.0,sigmaY: 10.0,),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child:
+                Column(
                   children: [
-                    Text(
-                      'Hello: Username',
-                      style: TextStyle(
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
                     SizedBox(
-                      width: 20.0,
+                      height: 20.0,
                     ),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20.0),
-                        color: Colors.green,
-                      ),
-                      height: 40.0,
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Contact Us',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 17.0,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                Stack(
-                  alignment: AlignmentDirectional.center,
-                  children: [
                     Stack(
-                      alignment: AlignmentDirectional.topCenter,
+                      alignment: AlignmentDirectional.center,
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                              30.0,
+                        Stack(
+                          alignment: AlignmentDirectional.topCenter,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  30.0,
+                                ),
+                              ),
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              child: Image(
+                                image: AssetImage('image/3.jpg'),
+                                height: 300.0,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          child: Image(
-                            image: AssetImage('image/3.jpg'),
-                            height: 350.0,
-                            fit: BoxFit.cover,
-                          ),
+                            Padding(
+                              padding: const EdgeInsetsDirectional.only(
+                                end: 10.0,
+                                start: 10.0,
+                                top: 50.0,
+                              ),
+                              child: Text(
+                                'Scan your plant to know the plant has disease or not?',
+                                style: TextStyle(
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                         Padding(
                           padding: const EdgeInsetsDirectional.only(
-                            end: 10.0,
-                            start: 10.0,
-                            top: 50.0,
+                            top: 40.0,
                           ),
-                          child: Text(
-                            'Scan your plant to know the plant has disease or not?',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsetsDirectional.only(
-                        top: 40.0,
-                      ),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          color: Colors.green,
-                        ),
-                        height: 40.0,
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(
-                              context,
-                              ScanarResult.screenRoute,
-                            );
-                          },
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
                             children: [
-                              Text(
-                                'UPLOAD IMAGE',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 17.0,
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Colors.green,
                                 ),
-                              ),
-                              SizedBox(
-                                width: 30.0,
-                              ),
-                              Icon(
-                                Icons.upload,
-                                color: Colors.white,
+                                height: 40.0,
+                                child: TextButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      ScanarResult.screenRoute,
+                                    );
+                                  },
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text(
+                                        'UPLOAD IMAGE',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 17.0,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 30.0,
+                                      ),
+                                      Icon(
+                                        Icons.upload,
+                                        color: Colors.white,
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ],
                           ),
                         ),
-                      ),
+                      ],
                     ),
                   ],
-                )
-              ],
+                ),
+              ),
             ),
           ),
         ),
